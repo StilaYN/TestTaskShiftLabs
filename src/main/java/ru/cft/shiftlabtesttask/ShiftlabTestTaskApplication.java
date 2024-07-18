7c0607c33a659bb6b0ef046d17bbda49fd36b215
StilaYN <dist.avdeev2004@gmail.com>
2024-07-18 21:09:07 +0700

    Добавлена пустышка spring-boot приложения

---
+++ src/main/java/ru/cft/shiftlabtesttask/ShiftlabTestTaskApplication.java
@@ -0,0 +1,12 @@
+package ru.cft.shiftlabtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftlabTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftlabTestTaskApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/cft/shiftlabtesttask/ShiftlabTestTaskApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.cft.shiftlabtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftlabTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
