2f9275128c21dfec0ce97895141247a181a3fd57
StilaYN <dist.avdeev2004@gmail.com>
2024-07-21 12:26:07 +0700

    Добавлен абстрактный класс для обозначения сервисов

---
+++ src/main/java/ru/cft/shiftlabtesttask/core/service/IntervalService.java
@@ -0,0 +1,14 @@
+package ru.cft.shiftlabtesttask.core.service;
+
+import ru.cft.shiftlabtesttask.api.IntervalKind;
+
+import java.util.List;
+
+public abstract class IntervalService<T> {
+
+    protected IntervalKind kind = null;
+
+    public abstract List<T> getMin();
+
+    public abstract List<T> merge(List<List<Object>> request);
+}
