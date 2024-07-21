7203dc98965a09fd308c3ce7cf445966549757df
StilaYN <dist.avdeev2004@gmail.com>
2024-07-21 22:55:24 +0700

    реализован маппер для числового интервала

---
+++ src/main/java/ru/cft/shiftlabtesttask/core/mapper/DigitIntervalMapper.java
@@ -0,0 +1,29 @@
+package ru.cft.shiftlabtesttask.core.mapper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.cft.shiftlabtesttask.core.entities.DigitInterval;
+import ru.cft.shiftlabtesttask.core.validator.DigitIntervalValidator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class DigitIntervalMapper {
+
+    private final DigitIntervalValidator digitIntervalValidator;
+
+    public List<DigitInterval> map(List<List<Object>> intervalsList) {
+        List<DigitInterval> digitIntervals = new ArrayList<>();
+        for (List<Object> interval : intervalsList) {
+             DigitInterval digitInterval = DigitInterval.builder()
+                .leftBorder((Integer) interval.get(0))
+                .rightBorder((Integer) interval.get(1))
+                .build();
+            digitIntervalValidator.valid(digitInterval);
+            digitIntervals.add(digitInterval);
+        }
+        return digitIntervals;
+    }
+}
