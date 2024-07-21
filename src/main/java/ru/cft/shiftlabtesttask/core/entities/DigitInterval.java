52ec7bdc91e0310d93c5334267e6ed448c9729c8
StilaYN <dist.avdeev2004@gmail.com>
2024-07-21 22:54:16 +0700

    реализован числовой интервалов

--- src/main/java/ru/cft/shiftlabtesttask/core/entities/DigitInterval.java
+++ src/main/java/ru/cft/shiftlabtesttask/core/entities/DigitInterval.java
@@ -16,11 +16,55 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class DigitInterval {
+public class DigitInterval implements Interval<Integer, DigitInterval> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Integer leftBorder;
     private Integer rightBorder;
+
+    @Override
+    protected Object clone() {
+        return DigitInterval.builder()
+            .leftBorder(leftBorder)
+            .rightBorder(rightBorder)
+            .build();
+    }
+
+    @Override
+    public int compareTo(DigitInterval o) {
+        if(o==null){
+            throw new NullPointerException("");
+        }
+        if(getLeftBorder()<o.getLeftBorder()){
+            return -1;
+        }
+        else if(getLeftBorder()>getRightBorder()){
+            return 1;
+        }
+        else{
+            return getRightBorder().compareTo(o.getRightBorder());
+        }
+    }
+
+    @Override
+    public DigitInterval combine(DigitInterval secondInterval) {
+        if (secondInterval == null) {
+            throw new NullPointerException("Нельзя объединить с пустым интервалом");
+        }
+        DigitInterval leftInterval = (getLeftBorder() < secondInterval.getLeftBorder()) ? this : secondInterval;
+        DigitInterval rightInterval = (leftInterval.equals(this)) ? secondInterval : this;
+        if (leftInterval.getLeftBorder().equals(rightInterval.getLeftBorder())
+            && leftInterval.getRightBorder().equals(rightInterval.getRightBorder())) {
+            return (DigitInterval) this.clone();
+        } else if (leftInterval.getRightBorder() < rightInterval.getLeftBorder()) {
+            return null;
+        } else
+            return DigitInterval.builder()
+                .leftBorder(leftInterval.getLeftBorder())
+                .rightBorder((leftInterval.getRightBorder() > rightInterval.getRightBorder())
+                    ? leftInterval.getRightBorder() : rightInterval.getRightBorder())
+                .build();
+    }
 }
