bc529c11b0723b75514282845dd25486d59973b1
StilaYN <dist.avdeev2004@gmail.com>
2024-07-21 12:27:17 +0700

    Добавлен валидатор для проверки, что передается только интервалы с двумя точками

---
+++ src/main/java/ru/cft/shiftlabtesttask/core/exception/IntervalFormatException.java
@@ -0,0 +1,8 @@
+package ru.cft.shiftlabtesttask.core.exception;
+
+public class IntervalFormatException extends RuntimeException{
+
+    public IntervalFormatException(String message) {
+        super(message);
+    }
+}
