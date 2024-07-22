c6c71ec478b70e0fbf6e138e81a280b8951a3d73
StilaYN <dist.avdeev2004@gmail.com>
2024-07-22 08:24:21 +0700

    добавлены проверка, что входной интервал состоит только из одного символа

--- src/main/java/ru/cft/shiftlabtesttask/core/mapper/CharacterIntervalMapper.java
+++ src/main/java/ru/cft/shiftlabtesttask/core/mapper/CharacterIntervalMapper.java
@@ -3,6 +3,7 @@ package ru.cft.shiftlabtesttask.core.mapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.cft.shiftlabtesttask.core.entities.CharInterval;
+import ru.cft.shiftlabtesttask.core.exception.IntervalFormatException;
 import ru.cft.shiftlabtesttask.core.validator.CharacterIntervalValidator;
 
 import java.util.ArrayList;
@@ -17,9 +18,14 @@ public class CharacterIntervalMapper {
     public List<CharInterval> map(List<List<Object>> intervalsList) {
         List<CharInterval> charIntervals = new ArrayList<>();
         for (List<Object> interval : intervalsList) {
+            String left = interval.get(0).toString();
+            String right = interval.get(1).toString();
+            if(left.length()!=1||right.length()!=1) {
+                throw new IntervalFormatException("левая и правая точка интервала должен состоять только из одного символа");
+            }
             CharInterval charInterval = CharInterval.builder()
-                .leftBorder(interval.get(0).toString().charAt(0))
-                .rightBorder(interval.get(1).toString().charAt(0))
+                .leftBorder(left.toLowerCase().charAt(0))
+                .rightBorder(right.toLowerCase().charAt(0))
                 .build();
             characterIntervalValidator.valid(charInterval);
             charIntervals.add(charInterval);
