2c875c55afbee03ce84faf244dc2b87bc901b0d7
StilaYN <dist.avdeev2004@gmail.com>
2024-07-22 08:04:11 +0700

    добавлено исключение, если в базе данных нет интервалов

---
+++ src/main/java/ru/cft/shiftlabtesttask/core/exception/IntervalNotFoundException.java
@@ -0,0 +1,8 @@
+package ru.cft.shiftlabtesttask.core.exception;
+
+public class IntervalNotFoundException extends RuntimeException {
+
+    public IntervalNotFoundException(String message) {
+        super(message);
+    }
+}
