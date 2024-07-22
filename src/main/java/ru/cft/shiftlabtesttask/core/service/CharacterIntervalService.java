2330c11759d6957f1dec44d0215fa06ba145d8e4
StilaYN <dist.avdeev2004@gmail.com>
2024-07-22 08:05:58 +0700

    реализован сервис для обработки запросов связанных  интервалами символов

--- src/main/java/ru/cft/shiftlabtesttask/core/service/CharacterIntervalService.java
+++ src/main/java/ru/cft/shiftlabtesttask/core/service/CharacterIntervalService.java
@@ -1,21 +1,39 @@
 package ru.cft.shiftlabtesttask.core.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.cft.shiftlabtesttask.api.IntervalKind;
+import ru.cft.shiftlabtesttask.core.entities.CharInterval;
+import ru.cft.shiftlabtesttask.core.exception.IntervalNotFoundException;
+import ru.cft.shiftlabtesttask.core.mapper.CharacterIntervalMapper;
+import ru.cft.shiftlabtesttask.core.repository.CharacterIntervalRepository;
 
 import java.util.List;
 
 @Service
-public class CharacterIntervalService extends IntervalService<Character> {
+@RequiredArgsConstructor
+public class CharacterIntervalService implements IntervalService<Character> {
 
-    protected IntervalKind kind = IntervalKind.letters;
+    private final CombiningIntervalService<CharInterval> combiningIntervalService;
+    private final CharacterIntervalMapper characterIntervalMapper;
+    private final CharacterIntervalRepository characterIntervalRepository;
+
+    @Override
+    public IntervalKind getKind() {
+        return IntervalKind.letters;
+    }
 
     @Override
     public List<Character> getMin() {
-        return List.of();
+        CharInterval result = characterIntervalRepository.findFirstByOrderByLeftBorderAscRightBorder()
+            .orElseThrow(() -> new IntervalNotFoundException("Интервал не был найден"));
+        return characterIntervalMapper.map(result);
     }
 
     @Override
     public void merge(List<List<Object>> request) {
+        List<CharInterval> charIntervalList = characterIntervalMapper.map(request);
+        List<CharInterval> charCombinedIntervalList = combiningIntervalService.combine(charIntervalList);
+        characterIntervalRepository.saveAll(charCombinedIntervalList);
     }
 }
