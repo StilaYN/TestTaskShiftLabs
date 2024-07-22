66d7dd8aeddee531569cfa8116c74ddaa2e3c656
StilaYN <dist.avdeev2004@gmail.com>
2024-07-22 08:07:25 +0700

    добавлены обработчики для конкретных типов исключений

--- src/main/java/ru/cft/shiftlabtesttask/api/controller/IntervalExceptionHandler.java
+++ src/main/java/ru/cft/shiftlabtesttask/api/controller/IntervalExceptionHandler.java
@@ -1,14 +1,27 @@
 package ru.cft.shiftlabtesttask.api.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.cft.shiftlabtesttask.core.exception.IntervalFormatException;
+import ru.cft.shiftlabtesttask.core.exception.IntervalNotFoundException;
 
 @RestControllerAdvice
 public class IntervalExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<Error> handle(Exception exception){
-        return ResponseEntity.status(400).body(new Error(exception.getMessage()));
+    public ResponseEntity<String> handle(IntervalFormatException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handle(IntervalNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handle(Exception exception){
+        return ResponseEntity.status(500).body(exception.getMessage());
     }
 }
