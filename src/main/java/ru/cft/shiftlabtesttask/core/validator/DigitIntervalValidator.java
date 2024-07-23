eb39795d36425af686e245e0b6efb0322c094fda
StilaYN <dist.avdeev2004@gmail.com>
2024-07-23 11:28:01 +0700

    Убраны ненужные части кода

--- src/main/java/ru/cft/shiftlabtesttask/core/validator/DigitIntervalValidator.java
+++ src/main/java/ru/cft/shiftlabtesttask/core/validator/DigitIntervalValidator.java
@@ -1,22 +1,12 @@
 package ru.cft.shiftlabtesttask.core.validator;
 
 import org.springframework.stereotype.Component;
-import ru.cft.shiftlabtesttask.core.entities.DigitInterval;
 import ru.cft.shiftlabtesttask.core.exception.IntervalFormatException;
 
 import java.util.List;
 
 @Component
 public class DigitIntervalValidator {
-
-    public void valid(DigitInterval interval) {
-        if (interval.getLeftBorder() > interval.getRightBorder()) {
-            throw new IntervalFormatException("Справа должно быть число больше чем слева");
-        }
-        if (interval.getLeftBorder().equals(interval.getRightBorder())) {
-            throw new IntervalFormatException("Слева и справа должны быть разные значения");
-        }
-    }
 
     public void validate(List<List<Object>> intervalList) {
         for (List<Object> interval : intervalList) {
